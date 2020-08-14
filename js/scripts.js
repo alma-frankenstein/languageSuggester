1092d520fa23afba254f9cae07e016380a07f281
Alma <a.q.frankenstein@gmail.com>
2020-08-14 08:50:42 -0700

    add second question

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
   $("form#survey").submit(function(event) {
     const kittenVal = parseInt($("input:radio[name=kitten]:checked").val());
-    
+    const jobVal = parseInt($("input:radio[name=job]:checked").val());
+
     event.preventDefault();
   });
 });
