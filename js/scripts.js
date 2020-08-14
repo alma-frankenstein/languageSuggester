9e15696330775911ae843e64d2ae760a2aa793a6
Alma <a.q.frankenstein@gmail.com>
2020-08-14 09:07:12 -0700

    add branching to sum answers and return suggested language

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
+function langChooser(total){
+  let lang;
+
+  if (total <= 10){
+    lang = "LOLCODE";
+  } else if (total >= 14){
+    lang = "Clojure"; 
+  } else {
+    lang = "Python";
+  }
+  return lang;
+}
+
+
 $(document).ready(function(){
   $("form#survey").submit(function(event) {
     const kittenVal = parseInt($("input:radio[name=kitten]:checked").val());
     const jobVal = parseInt($("input:radio[name=job]:checked").val());
+    const total = kittenVal + jobVal;
+
+    let language = langChooser(total);
+
+    $("#suggestedLanguage").show();
 
     event.preventDefault();
   });
